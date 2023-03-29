13ff08c87e3e5cf78dc7349f48a56a0efd74b422
fredymar <fredymartinezquinto@hotmail.com>
2023-03-29 18:21:51 -0500

    final marzo 28

--- hello-world.js
+++ hello-world.js
@@ -2,11 +2,15 @@ let nombre = "Michael"
 const deVariable = 324
 
 //console.log(nombre, deVariable)
-let valor = 5
+let valor = 3
 
 // operadores lógicos
 
-//=== comparar
+// = asignar
+
+// == es igual al contenido
+
+//=== comparar si es igual el contenido y el tipo de dato
 
 // > mayor
 
@@ -20,10 +24,19 @@ let valor = 5
 
 // || = ó
 
-if(valor > 0){
-    console.log("El valor es positivo")
-}else if(valor < 0){
-    console.log("EL valor es negativo")
-}else{
-    console.log("El valor es igual a 0")
+// si un numero es cero, positivo o negativo
+
+   /* if(valor > 0){
+        console.log("El valor es positivo")
+    }else if(valor < 0){
+        console.log("EL valor es negativo")
+    }else{
+        console.log("El valor es igual a 0 o no es un numero")
+    }*/
+// si un numero es par o impar
+
+if(valor % 2 === 0){
+    console.log("el numero es par")
+}else if( valor % 2 !== 0){
+    console.log("es impar")
 }
